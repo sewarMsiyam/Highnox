92ac6bad55a090161ca3eb52f2b7e0240d4009a3
sewarMsiyam <sewarsiyam@outlook.sa>
2023-08-05 22:11:48 +0300

    .

--- JS/dashboard/todo.js
+++ JS/dashboard/todo.js
@@ -141,32 +141,25 @@ function emptyTrash() {
 
 
 // color
-const colorPicker = document.getElementById('colorPicker');
-const colorNameInput = document.getElementById('colorName');
-const colorMap = new Map([
-['red', '#FF0000'],
-['green', '#00FF00'],
-['blue', '#0000FF'],
-['yellow', '#FFFF00'],
-['orange', '#FFA500'],
-['purple', '#800080'],
-['pink', '#FFC0CB'],
-['brown', '#A52A2A'],
-['gray', '#808080'],
-['black', '#000000'],
-['white', '#FFFFFF'],
-]);
+var colorList = [ '000000', '993300', '333300', '003300', '003366', '000066', '333399', '333333', 
+'660000', 'FF6633', '666633', '336633', '336666', '0066FF', '666699', '666666', 'CC3333', 'FF9933', '99CC33', '669966', '66CCCC', '3366FF', '663366', '999999', 'CC66FF', 'FFCC33', 'FFFF66', '99FF66', '99CCCC', '66CCFF', '993366', 'CCCCCC', 'FF99CC', 'FFCC99', 'FFFF99', 'CCffCC', 'CCFFff', '99CCFF', 'CC99FF', 'FFFFFF'  ];
+    var picker = $('#color-picker');
 
-colorPicker.addEventListener('input', (event) => {
-  const selectedColor = event.target.value;
-  colorNameInput.value = selectedColor;
-  colorNameInput.style.backgroundColor = selectedColor;
-});
-colorNameInput.addEventListener('input', (event) => {
-  const colorName = event.target.value.toLowerCase();
-  if (colorMap.has(colorName)) {
-    const colorValue = colorMap.get(colorName);
-    colorPicker.value = colorValue;
-    colorNameInput.style.backgroundColor = colorValue;
-  }
-});
+    for (var i = 0; i < colorList.length; i++ ) {
+      picker.append('<li class="color-item" data-hex="' + '#' + colorList[i] + '" style="background-color:' + '#' + colorList[i] + ';"></li>');
+    }
+
+    $('body').click(function () {
+      picker.fadeOut();
+    });
+
+    $('.call-picker').click(function(event) {
+      event.stopPropagation();
+      picker.fadeIn();
+      picker.children('li').hover(function() {
+        var codeHex = $(this).data('hex');
+
+        $('.color-holder').css('background-color', codeHex);
+        $('#pickcolor').val(codeHex);
+      });
+    });
