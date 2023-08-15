6ee489ef1d46167b70158527a526afd84d497e0e
sewarMsiyam <sewarsiyam@outlook.sa>
2023-08-15 13:33:42 +0300

    .

--- JS/dashboard/drag.js
+++ JS/dashboard/drag.js
@@ -4,21 +4,24 @@ dragula([
     document.getElementById("table3"),
     document.getElementById("table4"),
     document.getElementById("table5")
-  ]);
-  removeOnSpill: false
-    .on("drag", function (el) {
-      el.className.replace("ex-moved", "");
-    })
-    .on("drop", function (el) {
-      el.className += "ex-moved";
-    })
-    .on("over", function (el, container) {
-      container.className += "ex-over";
-    })
-    .on("out", function (el, container) {
-      container.className.replace("ex-over", "");
-    });
-  // delete item
-  function emptyTrash() {
-    document.getElementsById("task").innerHTML = "";
-  }
+  ], {
+    removeOnSpill: false
+  })
+  .on("drag", function (el) {
+    el.className.replace("ex-moved", "");
+  })
+  .on("drop", function (el, target, source) {
+    el.className += " ex-moved";
+    alert("Item dropped into destination table: " + target.id);
+  })
+  .on("over", function (el, container) {
+    container.className += " ex-over";
+  })
+  .on("out", function (el, container) {
+    container.className.replace(" ex-over", "");
+  });
+
+// delete item
+function emptyTrash() {
+  document.getElementById("task").innerHTML = "";
+}
--- JS/layout.js
+++ JS/layout.js
@@ -66,20 +66,62 @@
 
 
 //multiple select
-  $('.selectpicker').selectpicker();
-  $('.selectpicker').on('changed.bs.select', function (e) {
-    const selectedItems = $(this).val();
-    const selectedCount = selectedItems.length;
+  // $('.selectpicker').selectpicker();
+  // $('.selectpicker').on('changed.bs.select', function (e) {
+  //   const selectedItems = $(this).val();
+  //   const selectedCount = selectedItems.length;
+  //   if (selectedCount >= 3) {
+  //     $(this).parent().find('.filter-option-inner-inner').html(`<span style="color: #495057 !important;">${selectedCount} options selected</span>`);
+  //   }  
+  // });
 
+  $(document).ready(function() {
+    let isDropdownOpen = false;
   
-    if (selectedCount >= 3) {
-      $(this).parent().find('.filter-option-inner-inner').html(`<span style="color: #495057 !important;">${selectedCount} options selected</span>`);
-    }
+    $('.selectpicker').selectpicker();
+  
+    $('.selectpicker').on('changed.bs.select', function (e) {
+      const selectedItems = $(this).val();
+      const selectedCount = selectedItems.length;
+      if (selectedCount >= 3) {
+        $(this).parent().find('.filter-option-inner-inner').html(`<span style="color: #495057 !important;">${selectedCount} options selected</span>`);
+      }
+    });
+    
+    $('.selectpicker').on('show.bs.select', function () {
+      isDropdownOpen=true;
+      console.log("show");
+    });
+
+    $('.selectpicker').on('shown.bs.select', function (e) {
+      console.log("shown");
+      isDropdownOpen=true;
+      if(isDropdownOpen){
+        $('input').on('click', function (e) {
+          e.preventDefault();
+          $('.selectpicker').selectpicker('toggle');
+          isDropdownOpen=false;
+        });
+      }else{
+        $('.selectpicker').selectpicker('toggle');
+        isDropdownOpen=false;
+
+      }
+      e.preventDefault();
+    });
+
+
+    $('.selectpicker').on('hide.bs.select', function () {
+      console.log("hide");
+      isDropdownOpen=false;
+    });
+
+    $('.selectpicker').on('hidden.bs.select', function () {
+      console.log("hidden");
+      isDropdownOpen=false;
+    });
+  
   });
-
-  
-
-
 
 // toggle menu in sidebar
 var toggleSliderMenu = document.querySelectorAll(".display-menu");
