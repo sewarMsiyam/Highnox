025d09c0b153d3e455869f214af72c92a7c3b8b9
sewarMsiyam <sewarsiyam@outlook.sa>
2023-08-19 05:19:24 +0300

    .

--- JS/dashboard/main.js
+++ JS/dashboard/main.js
@@ -14,33 +14,33 @@
 // });
 
 
-// const downloadPdfActivityBtnWord = document.getElementById("download-contract-word");
-// downloadPdfActivityBtnWord.addEventListener("click", () => {
-//   downloadWordFile();
-// });
+const downloadPdfActivityBtnWord = document.getElementById("download-contract-word");
+downloadPdfActivityBtnWord.addEventListener("click", () => {
+  downloadWordFile();
+});
 
-// function downloadWordFile() {
-//   var fileUrl = 'path/to/your/file.docx';
-//   var link = document.createElement('a');
-//   link.href = fileUrl;
-//   link.download = 'filename.docx';
-//   link.click();
-// }
+function downloadWordFile() {
+  var fileUrl = 'path/to/your/file.docx';
+  var link = document.createElement('a');
+  link.href = fileUrl;
+  link.download = 'filename.docx';
+  link.click();
+}
 
 
 
-// const downloadActivityBtnExcel = document.getElementById("download-contract-excel");
-// downloadActivityBtnExcel.addEventListener("click", () => {
-//   downloadFile();
-// });
+const downloadActivityBtnExcel = document.getElementById("download-contract-excel");
+downloadActivityBtnExcel.addEventListener("click", () => {
+  downloadFile();
+});
 
-// function downloadFile() {
-//   var fileUrl = 'path/to/your/file.xlsx';
-//   var link = document.createElement('a');
-//   link.href = fileUrl;
-//   link.download = 'filename.xlsx';
-//   link.click();
-// }
+function downloadFile() {
+  var fileUrl = 'path/to/your/file.xlsx';
+  var link = document.createElement('a');
+  link.href = fileUrl;
+  link.download = 'filename.xlsx';
+  link.click();
+}
 
 
 // var toggleSliderMenuContractArrow = document.querySelector(".contract-sidebar");
@@ -309,50 +309,6 @@ function openFile(btn) {
     // put the value in the btn content
     btn.textContent = file.name;
   });
-}
-function sortTable(n, table_id) {
-  var table,
-    rows,
-    switching,
-    i,
-    x,
-    y,
-    shouldSwitch,
-    dir,
-    switchcount = 0;
-  table = document.getElementById(table_id);
-  switching = true;
-  dir = "asc";
-  while (switching) {
-    switching = false;
-    rows = table.rows;
-    for (i = 1; i < rows.length - 1; i++) {
-      shouldSwitch = false;
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      switchcount++;
-    } else {
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
 }
 
 // const copyIcons = document.querySelectorAll(".copy-icon");
--- JS/layout.js
+++ JS/layout.js
@@ -64,6 +64,11 @@
   });
 
 
+//date 
+ // $(".datepicker").datepicker();
+  $(".datepicker").datepicker();
+
+
 
 //multiple select
   $('.selectpicker').selectpicker();
@@ -150,17 +155,221 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
 
 // import list
-const fileInput = document.getElementById('fileInput');
-fileInput.addEventListener('change', (event) => {
-  const selectedFile = event.target.files[0];
-  if (selectedFile) {
-    const sewarDiv = document.getElementById('namefile');
-    sewarDiv.textContent = selectedFile.name;
-    console.log('Selected file path:', selectedFile.name); 
+// const fileInput = document.getElementById('fileInput');
+// fileInput.addEventListener('change', (event) => {
+//   const selectedFile = event.target.files[0];
+//   if (selectedFile) {
+//     const sewarDiv = document.getElementById('namefile');
+//     sewarDiv.textContent = selectedFile.name;
+//     console.log('Selected file path:', selectedFile.name); 
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+// الترتيب
+function sortTable(n, table_id) {
+  var table,
+    rows,
+    switching,
+    i,
+    x,
+    y,
+    shouldSwitch,
+    dir,
+    switchcount = 0;
+  table = document.getElementById(table_id);
+  switching = true;
+  dir = "asc";
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    for (i = 1; i < rows.length - 1; i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      switchcount++;
+    } else {
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
   }
+}
+
+
+
+
+
+
+$(document).ready(function() {
+  var countSelect=0;
+  $('.selectAll').on('change', function() {
+    var $table = $(this).closest('table');
+    var $checkboxes = $table.find('tbody input[type="checkbox"]');
+    $checkboxes.prop('checked', $(this).is(':checked'));
+    updateActionVisibility($table);
+    updateSelectAll($table);
+  });
+  $('table input[type="checkbox"]').on('change', function() {
+    var $table = $(this).closest('.table');
+    updateActionVisibility($table);
+    updateSelectAll($table);
+  });
+
+  function updateSelectAll($table) {
+    var $selectAll = $table.find('.selectAll');
+    var $checkboxes = $table.find('tbody input[type="checkbox"]');
+    $selectAll.prop('checked', $checkboxes.length === $checkboxes.filter(':checked').length);
+  }
+
+  function updateActionVisibility($table) {
+    var $actionContainer = $table.closest('.table-tab-pane').find('.action-container');
+    var $deleteButton = $actionContainer.find('.delete-selected'); 
+    var $selectedCount = $actionContainer.find('.selected-count');
+    var $selectedCheckboxes = $table.find('tbody input[type="checkbox"]:checked');
+    $actionContainer.toggleClass('d-none', $selectedCheckboxes.length === 0);
+    $deleteButton.prop('disabled', $selectedCheckboxes.length === 0);
+    $selectedCount.text($selectedCheckboxes.length);
+    $selectedCount.toggleClass('d-none', $selectedCheckboxes.length === 0);
+    countSelect= $selectedCheckboxes.length;
+  }
+
+  $('.delete-selected').on('click', function() {
+    alert('delete  count ' + countSelect );
+  });
+
 });
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var fileInputWord = $('.file-word-input');
+var downloadContainerWord = $('#import-contract-word');
+downloadContainerWord.on('click', function() {
+  fileInputWord.trigger('click');
+});
+fileInputWord.on('change', function() {
+    var allowedExtensionsWord = /(\.docx|\.doc)$/i;
+    if (allowedExtensionsWord.exec(fileInputWord[0].files[0].name)) {
+        $('#name_file_word').text(fileInputWord[0].files[0].name);
+        var fileSizeBytes = fileInputWord[0].files[0].size;
+        var fileSizeGB = (fileSizeBytes / (1024 * 1024 * 1024)).toFixed(3);
+        $('#gbs_file_word').text(fileSizeGB + ' gbs');
+
+        // تحديث عنوان الملف المرفوع
+        var uploadedFileName = fileInputWord[0].files[0].name;
+        updateDownloadLink(uploadedFileName);
+        const downloadPdfActivityBtnWord = document.getElementById("download-contract-word");
+          downloadPdfActivityBtnWord.addEventListener("click", () => {
+              downloadWordFile();
+          });
+          function updateDownloadLink(uploadedFileName) {
+              var downloadLink = $('#download-contract-word');
+              downloadLink.attr('data-file-name', uploadedFileName);
+          }
+          function downloadWordFile() {
+              var downloadLink = $('#download-contract-word');
+              var uploadedFileName = downloadLink.attr('data-file-name');
+              var fileUrl =  uploadedFileName;
+              var link = document.createElement('a');
+              link.href = fileUrl;
+              link.download = uploadedFileName;
+              link.click();
+          }
+    } else {
+        alert('Please select a valid Word document file.');
+        fileInputWord.val('');
+        $('#name_file_word').text('select file word');
+        $('#gbs_file_word').text('0 gbs');
+    }
+});
+
+var fileInputExcel = $('.file-excel-input');
+var downloadContainerExcel = $('#import-contract-excel');
+downloadContainerExcel.on('click', function() {
+  fileInputExcel.trigger('click');
+});
+fileInputExcel.on('change', function() {
+    var allowedExtensionsExcel = /(\.xlsx|\.xls)$/i;
+    if (allowedExtensionsExcel.exec(fileInputExcel[0].files[0].name)) {
+        $('#name_file_excel').text(fileInputExcel[0].files[0].name);
+        var fileSizeBytes = fileInputExcel[0].files[0].size;
+        var fileSizeGB = (fileSizeBytes / (1024 * 1024 * 1024)).toFixed(3);
+        $('#gbs_file_excel').text(fileSizeGB + ' gbs');
+
+        // تحديث عنوان الملف المرفوع
+        var uploadedFileName = fileInputExcel[0].files[0].name;
+        updateDownloadLink(uploadedFileName);
+        const downloadPdfActivityBtnExcel = document.getElementById("download-contract-excel");
+          downloadPdfActivityBtnExcel.addEventListener("click", () => {
+              downloadExcelFile();
+          });
+          function updateDownloadLink(uploadedFileName) {
+              var downloadLink = $('#download-contract-excel');
+              downloadLink.attr('data-file-name', uploadedFileName);
+              downloadButton.on('click', function() {
+                var fileUrl = 'path/to/your/uploaded/files/' + uploadedFileName;
+                window.open(fileUrl, '_blank'); // فتح في تاب جديدة
+            });
+          }
+          function downloadExcelFile() {
+              var downloadLink = $('#download-contract-excel');
+              var uploadedFileName = downloadLink.attr('data-file-name');
+              var fileUrl =  uploadedFileName;
+              var link = document.createElement('a');
+              link.href = fileUrl;
+              link.download = uploadedFileName;
+              link.click();
+              
+          }
+    } else {
+        alert('Please select a valid excel document file.');
+        fileInputExcel.val('');
+        $('#name_file_excel').text('select file excel');
+        $('#gbs_file_excel').text('0 gbs');
+    }
+});
